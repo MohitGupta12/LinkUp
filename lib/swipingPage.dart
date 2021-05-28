0162a57c393879eaee77c1c0b6b54b7632893692
Mohit Gupta <mohit2412gupta@gmail.com>
2021-05-28 15:15:12 +0530

    Added hobbies button and completed passion page

---
+++ lib/swipingPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Swiping extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [],
+      ),
+    );
+  }
+}
